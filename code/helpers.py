653cc9154cca792e631100453d1dcd709d89d1ad
chanjrt <timjrchan89@gmail.com>
2024-07-02 17:04:36 +0800

    included VIF test

--- code/helpers.py
+++ code/helpers.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+from sklearn.model_selection import cross_validate
 
 
 def clean_headers(some_dataframe: pd.DataFrame):
@@ -22,5 +22,23 @@ def clean_headers(some_dataframe: pd.DataFrame):
     some_dataframe = some_dataframe.drop(columns='id')
 
     return some_dataframe
+
+
+# Function to get CV results out
+
+def get_cv_results(model, X,y, no_of_folds):
+
+    '''
+    
+    '''
+
+    cv_results = cross_validate(model, X, y, cv = no_of_folds,
+                                scoring = ('neg_mean_absolute_error', 'neg_mean_squared_error', 'r2'),
+                                return_train_score = True)
+    
+    print('Cross Validation Results:')
+
+    for k,v in cv_results.items():
+        print(k,v)
 
 
